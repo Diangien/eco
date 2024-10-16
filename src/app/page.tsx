8fe54ee1d7d59c3e8e0b3cd4f6bdd20eab251ce0
Diangien <106443686+Diangien@users.noreply.github.com>
2024-10-16 22:09:33 +0100

    Adicionei o banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,27 @@ export default function Page(){
     <>
     
       {/* Inicio da HEADER */}
-
       <Header/>
       {/* Fim da HEADER */}
+
+      {/* Eventos Recentes */}
+
+      <div className="eventoContainer">
+        <div className="evento bg-[url('/l1_novo.jpeg')] bg-cover bg-no-repeat h-60 md:h-[30rem] bg-center flex items-center justify-start px-4">
+          <div>
+            <em className="font-thin text-white text-xl md:text-4xl">12 de SETEMBRO</em>
+            <br />
+            <br />
+            <strong className="text-white text-xl md:text-4xl">LIMPEZA DA PRAIA <br />EM LUANDA</strong>
+            <br />
+            <br />
+            <button className="bg-green-600 text-gray-100 rounded-2xl px-14 py-2 transition  hover:bg-gray-100 hover:text-green-600 hover:font-bold">Inscrever-se</button>
+          </div>
+          
+        </div>
+      </div>
+
+      {/* Fim Eventos Recentes */}
     
     </>
   )
